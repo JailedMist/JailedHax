4f73dbc5e7b96de25f31ae5c373b143ac9340327
JailedMist <65553073+JailedMist@users.noreply.github.com>
2020-06-24 11:39:24 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 if secure_load and "Sentinel" then 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JailedMist/JailedHax/master/Sentinel", true))()
-elseif pebc_execute and "ProtoSmasher"" then
+elseif PROTOSMASHER_LOADED then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JailedMist/JailedHax/master/jailedHax%20free", true))()
 elseif is_sirhurt_closure and " Sirhurt" or is_synapse_function and "SynapseX" then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JailedMist/JailedHax/master/Other", true))()
